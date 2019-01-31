588fd877e66bed7b6b6cffab2f674268ce8193d1
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-31 07:04:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  attr_accessor :word
+  def match(list)
+    word_arr = @word.split("")
+    word_arr.sort!
+    final = []
+    
+    list.each do |x|
+      will = x.split("")
+      will.sort!
+      if will == word_arr
+        final << x
+      end
+    end
+    return final
+  end
+end
