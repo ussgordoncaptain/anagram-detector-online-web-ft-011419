de1b14cc6ba0d02b8fa3173d8cd35d3a9a8649b9
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-31 06:44:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  attr_accessor :word
+  def match(list)
+    word_arr = @word.split("")
+    word_arr.sort!
+    final = []
+    
+    list.each do |x|
+  end
+end
